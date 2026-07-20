67a5c2a4f642e69165f36dfe303a62999ebb2767
martin <martin.iungman@gmail.com>
2026-07-20 15:27:58 -0300

    Fix x-axis label collision in R6 scatter plots
    Facet x-axis text (0/30/60/90/120) was colliding between Rep 1 and
    Rep 2 panels and getting clipped at the right edge - text size is 30pt
    in a fairly narrow facet. Fixed with panel.spacing, fewer breaks
    (0/50/100) and more right-side expansion, plus a bit more width.

--- R/14_tata_cgi/tata_cgi.R
+++ R/14_tata_cgi/tata_cgi.R
@@ -47,9 +47,10 @@ motif_scatter <- function(data, motif, y_label, title) {
     geom_point(col = "#14AFB2") +
     geom_smooth(col = "#216869") +
     theme_pubclean() +
-    theme(text = element_text(size = 30)) +
+    theme(text = element_text(size = 30), panel.spacing = unit(3, "lines")) +
     facet_wrap(~rep) +
     ylim(0, 1) +
+    scale_x_continuous(breaks = c(0, 50, 100), expand = expansion(mult = c(0.03, 0.1))) +
     labs(x = "Bins de actividad media", y = y_label) +
     ggtitle(title)
 }
@@ -70,7 +71,7 @@ violin_by_motif <- function(data, rep_id, motif, fill_label) {
 # --- TATA-box ---------------------------------------------------------
 
 panel_tata_scatter <- motif_scatter(data, "TATA_EPD", "Proporción de promotores\ncon TATA-box", "TATA-box")
-ggsave(file.path(out_dir, "tata_scatter.jpg"), panel_tata_scatter, width = 9, height = 6.75, units = "in")
+ggsave(file.path(out_dir, "tata_scatter.jpg"), panel_tata_scatter, width = 12, height = 6.75, units = "in")
 
 ggsave(file.path(out_dir, "tata_violin_rep1.jpg"), violin_by_motif(data, "Rep 1", "TATA_EPD", "TATA-box"), width = 9, height = 6.75, units = "in")
 ggsave(file.path(out_dir, "tata_violin_rep2.jpg"), violin_by_motif(data, "Rep 2", "TATA_EPD", "TATA-box"), width = 9, height = 6.75, units = "in")
@@ -78,7 +79,7 @@ ggsave(file.path(out_dir, "tata_violin_rep2.jpg"), violin_by_motif(data, "Rep 2"
 # --- Isla CpG -----------------------------------------------------------
 
 panel_cgi_scatter <- motif_scatter(data, "CGI", "Proporción de promotores\ncon islas CpG", "Isla CpG")
-ggsave(file.path(out_dir, "cgi_scatter.jpg"), panel_cgi_scatter, width = 9, height = 6.75, units = "in")
+ggsave(file.path(out_dir, "cgi_scatter.jpg"), panel_cgi_scatter, width = 12, height = 6.75, units = "in")
 
 ggsave(file.path(out_dir, "cgi_violin_rep1.jpg"), violin_by_motif(data, "Rep 1", "CGI", "Isla CpG"), width = 9, height = 6.75, units = "in")
 ggsave(file.path(out_dir, "cgi_violin_rep2.jpg"), violin_by_motif(data, "Rep 2", "CGI", "Isla CpG"), width = 9, height = 6.75, units = "in")
@@ -86,7 +87,7 @@ ggsave(file.path(out_dir, "cgi_violin_rep2.jpg"), violin_by_motif(data, "Rep 2",
 # --- CCAAT-box ------------------------------------------------------------
 
 panel_ccaat_scatter <- motif_scatter(data, "CCAAT_EPD", "Proporción de promotores\ncon CCAAT-box", "CCAAT-box")
-ggsave(file.path(out_dir, "ccaat_scatter.jpg"), panel_ccaat_scatter, width = 9, height = 6.75, units = "in")
+ggsave(file.path(out_dir, "ccaat_scatter.jpg"), panel_ccaat_scatter, width = 12, height = 6.75, units = "in")
 
 ggsave(file.path(out_dir, "ccaat_violin_rep1.jpg"), violin_by_motif(data, "Rep 1", "CCAAT_EPD", "CCAAT-box"), width = 9, height = 6.75, units = "in")
 ggsave(file.path(out_dir, "ccaat_violin_rep2.jpg"), violin_by_motif(data, "Rep 2", "CCAAT_EPD", "CCAAT-box"), width = 9, height = 6.75, units = "in")
